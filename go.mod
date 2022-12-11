dad0161eff2a9f7076d9c59545c9bbca3935bbf9
orion <oletizi@mac.com>
2022-12-11 15:55:36 -0800

    - futzing with tview

--- go.mod
+++ go.mod
@@ -3,12 +3,21 @@ module github.com/oletizi/samplemgr
 go 1.18
 
 require (
+	github.com/gdamore/tcell/v2 v2.5.3
+	github.com/rivo/tview v0.0.0-20221211160934-55d1ca9d3392
 	github.com/stretchr/testify v1.8.1
 	golang.org/x/exp v0.0.0-20221208152030-732eee02a75a
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
+	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main/main.go
+++ main/main.go
@@ -1,26 +1,50 @@
-/*
- * Copyright (c) 2022 Orion Letizi
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package main
 
 import (
 	"fmt"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+	"log"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	boxLeft := tview.NewBox().SetBorder(true).SetTitle("Left Side")
+	boxRight := tview.NewBox().SetBorder(true).SetTitle("Right Side")
+	logView := tview.NewTextView()
+	logView.SetBorder(true).SetTitle("Log")
+	logView.Clear()
+	logger := &Logger{view: logView}
+
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexColumn).
+		AddItem(boxLeft, 0, 1, true).
+		AddItem(boxRight, 0, 1, true).
+		AddItem(logView, 0, 1, true)
+
+	app := tview.NewApplication()
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		logger.Println(fmt.Sprintf("Event name: %s, rune: %d", event.Name(), event.Rune()))
+		return event
+	})
+	if err := app.SetRoot(flex, true).Run(); err != nil {
+		panic(err)
+	}
+}
+
+type Logger struct {
+	view *tview.TextView
+}
+
+func (l *Logger) Println(msg any) {
+	w := l.view.BatchWriter()
+	defer func(w tview.TextViewWriter) {
+		err := w.Close()
+		if err != nil {
+			log.Print(err)
+		}
+	}(w)
+	_, err := fmt.Fprintln(w, msg)
+	if err != nil {
+		log.Print(err)
+	}
 }
